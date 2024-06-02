e4599d7405d9ff27261f186a9d77e1db5dd54b1a
CrambitHazard <143696982+CrambitHazard@users.noreply.github.com>
2024-06-03 02:52:56 +0530

    Add files via upload

--- neural.py
+++ neural.py
@@ -12,6 +12,18 @@ np.random.shuffle(data) # randomizing as a pre-step for cross-validation
 
 data_dev=data[0:1000].T # creating our cross validation data to prevent overfitting, also the .T is for easier access in future steps i.e. each column is an example rather than each row
 # print(data_dev)
-
 Y_dev=data[0] # first row contains all the examples, i.e. numbers
 X_dev=data[1:n] # rest of the rows contain the features, i.e. the values of the pixels
+
+data_train=data[1000:m].T  # creating our transposed training dataset
+Y_train=data_train[0]
+X_train=data_train[1:n]
+
+
+def init_params():
+    # initializing all parameters for the formulas/math
+    W1=np.random.randn(10,784) - 0.5 # weights for input layer to hidden layer, -0.5 to make the range [-0.5,0.5] rather than [0,1] as this will be helpful with ReLU function
+    b1=np.random.randn(10,1) - 0.5 # biases for input layer to hidden layer
+    W2=np.random.randn(10,10) - 0.5 # weights for hidden layer to output layer
+    b2=np.random.randn(10,1) - 0.5 # biases for hidden layer to output layer
+    return W1,b1,W2,b2
